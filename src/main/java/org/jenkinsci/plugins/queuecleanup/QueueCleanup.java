15dc723bbcd630b730304f6f0dca2c57d2fb99f1
Oliver Gondža <ogondza@redhat.com>
2014-07-21 13:10:16 +0200

    Polish

--- src/main/java/org/jenkinsci/plugins/queuecleanup/QueueCleanup.java
+++ src/main/java/org/jenkinsci/plugins/queuecleanup/QueueCleanup.java
@@ -6,6 +6,7 @@ import hudson.model.PeriodicWork;
 import hudson.model.Descriptor;
 import hudson.model.Queue;
 
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import jenkins.model.Jenkins;
@@ -15,11 +16,14 @@ import org.kohsuke.stapler.StaplerRequest;
 
 @Extension
 public class QueueCleanup extends PeriodicWork implements Describable<QueueCleanup> {
-    
+
     private static final Logger LOGGER = Logger.getLogger(QueueCleanup.class.getName());
-    
+
+    /**
+     * Check period in minutes.
+     */
     private static final Integer QUEUE_CLEANUP_PERIOD;
-    
+
     static {
         Integer period = new Integer(1440); // default 24 hours
         try {
@@ -28,9 +32,9 @@ public class QueueCleanup extends PeriodicWork implements Describable<QueueClean
              LOGGER.warning(String.format("Cannot convert string %s to integer, using dafault %d", System.getProperty(QueueCleanup.class.getName()+".period"), period));
         } finally {
             QUEUE_CLEANUP_PERIOD = period;
-        } 
+        }
     }
-    
+
     @Override
     public long getRecurrencePeriod() {
         return QUEUE_CLEANUP_PERIOD*HOUR;
@@ -38,7 +42,7 @@ public class QueueCleanup extends PeriodicWork implements Describable<QueueClean
 
     @Override
     protected void doRun() throws Exception {
-        System.out.println("Queue clenaup called! Timeout is " + getDescriptor().getTimeout());
+        LOGGER.log(Level.INFO, "Queue clenaup started. Timeout is " + getDescriptor().getTimeout());
         Queue queue = Jenkins.getInstance().getQueue();
         Queue.Item[] items = queue.getItems();
         long currTime = System.currentTimeMillis();
@@ -46,34 +50,36 @@ public class QueueCleanup extends PeriodicWork implements Describable<QueueClean
         String pattern = getDescriptor().getItemPattern();
         for(Queue.Item item : items) {
             long inQueue = currTime - item.getInQueueSince();
-            if(inQueue > timeoutMillis && item.task.getDisplayName().matches(pattern)) 
+            if(inQueue > timeoutMillis && item.task.getDisplayName().matches(pattern)) {
                 queue.cancel(item);
+                LOGGER.log(Level.WARNING, "Item '{}'removed from queue after", new String[] {item.task.getFullDisplayName()});
+            }
         }
     }
-    
+
     public DescriptorImpl getDescriptor() {
         return (DescriptorImpl)Jenkins.getInstance().getDescriptorOrDie(getClass());
     }
-    
+
     @Extension
     public static final class DescriptorImpl extends Descriptor<QueueCleanup> {
 
         private int timeout = 24; //task timeout in the queue - in hours
         private String itemPattern = "*";
-        
+
         public DescriptorImpl() {
             super(QueueCleanup.class);
             load();
         }
-        
+
         public int getTimeout() {
             return timeout;
         }
-        
+
         public String getItemPattern() {
             return itemPattern;
         }
-        
+
         @Override
         public boolean configure(StaplerRequest req, JSONObject formData) throws FormException {
             try {
@@ -82,16 +88,14 @@ public class QueueCleanup extends PeriodicWork implements Describable<QueueClean
                 throw new FormException("Cannot convert to integer - timeout has to be integer!", "timeout");
             }
             this.itemPattern = req.getParameter("queueCleanup.itemPattern");
-            
+
             save();
             return true;
         }
-        
+
         @Override
         public String getDisplayName() {
             return "Queue cleanup";
         }
     }
-    
-
 }
